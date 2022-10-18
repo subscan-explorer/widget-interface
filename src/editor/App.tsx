e92aaa0264b8fd6497a9b44715ac394011b27334
WoeOm <1015996366@qq.com>
2022-10-18 10:00:23 +0800

    fix: add baseURL

--- src/editor/App.tsx
+++ src/editor/App.tsx
@@ -1,41 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Editor, Record } from 'editor';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ThemeProvider } from 'styled-components';
 import { light } from 'ui/theme';
 import { theme } from 'ui/chakraTheme';
-import { fetchConfigById } from './services';
-
-const router = createBrowserRouter([
-  {
-    path: "/editor",
-    element: <Editor />,
-
-    children: [
-      {
-        element: <Editor />,
-        path: ":appId",
-        id: "editor",
-        loader: async ({ params }) => {
-          try {
-            if (params.appId) {
-              return await fetchConfigById(params.appId);
-            }
-          } catch (error) {
-            console.log('/editor loader', error);
-            return {};
-          }
-        }
-      }
-    ]
-  },
-  {
-    path: "/",
-    element: <Record />,
-  },
-]);
+import { router } from './router';
 
 function App() {
   return (<ChakraProvider resetCSS={false} theme={theme}>
