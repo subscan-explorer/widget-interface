744a7f0de21b5675f3e9bb2e886957c240fe4758
WoeOm <1015996366@qq.com>
2022-12-16 18:00:26 +0800

    feat: more icons

--- src/components/Svg/Icons.tsx
+++ src/components/Svg/Icons.tsx
@@ -3,6 +3,12 @@ import * as IconsList from './index';
 export const Icons = {
   barchart: IconsList.BarChartIcon,
   finalized: IconsList.FinalizedIcon,
+  note: IconsList.NoteIcon,
+  pserson: IconsList.PersonIcon,
+  points: IconsList.PointsIcon,
+  rate: IconsList.RateIcon,
+  stake: IconsList.StakeIcon,
+  swap: IconsList.SwapIcon,
 };
 
 export type IconName = keyof typeof Icons;
