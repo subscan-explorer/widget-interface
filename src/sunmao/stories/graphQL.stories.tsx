0ed95e47b1d1d9912d5d46bc8c915f9dd1b90ae5
WoeOm <1015996366@qq.com>
2022-12-19 15:16:22 +0800

    feat: datasource stories

---
+++ src/sunmao/stories/graphQL.stories.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Application } from '@subscan/widget-core';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Preview from 'preview';
+import { initChainStyles } from 'config/stylebook';
+
+const AppOptions: Application = {
+  "version": "widget/v1",
+  "kind": "Application",
+  "metadata": {
+    "name": "subscan widget"
+  },
+  "spec": {
+    "components": [
+      {
+        "id": "title1",
+        "type": "core/v1/title",
+        "properties": {
+          "text": "{{JSON.stringify(graphQL0.data)}}"
+        },
+        "traits": [
+          {
+            "type": "core/v1/event",
+            "properties": {
+              "handlers": [
+                {
+                  "type": "$onMount",
+                  "componentId": "api0",
+                  "method": {
+                    "name": "triggerFetch",
+                    "parameters": {}
+                  },
+                  "wait": {
+                    "type": "debounce",
+                    "time": 0
+                  },
+                  "disabled": false
+                }
+              ]
+            }
+          }
+        ]
+      },
+      {
+        "id": "graphQL0",
+        "type": "core/v1/dummy",
+        "properties": {},
+        "traits": [
+          {
+            "type": "core/v1/graphQL",
+            "properties": {
+              "url": "https://api.subquery.network/sq/itering/multisig-polkadot",
+              "query": "query {\n    approveRecords (first: 5) {\n        nodes {\n            id\n            multisigRecordId\n            account\n            approveTimepoint\n        }\n    }\n    multisigRecords (first: 5) {\n        nodes {\n            id\n            createExtrinsicIdx\n            module\n            method\n        }\n    }\n}",
+              "variables": "{{{}}}",
+              "key": "value"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+export default {
+  title: 'Subscan Lib/DataSource GraphQL',
+  component: Preview,
+  layout: 'fullscreen',
+} as ComponentMeta<typeof Preview>;
+
+const Template: ComponentStory<typeof Preview> = (args) => <Preview {...args} />;
+
+export const Index = Template.bind({});
+
+Index.args = {
+  options: AppOptions,
+  ...initChainStyles
+};
